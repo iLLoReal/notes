7adbd4f9fbd079d706fd4c47e97edb4bb7420a37
iLLoReal <olivier.laffon.dev@gmail.com>
2023-03-05 01:02:57 +0100

    Ajout retour à la ligne; Ajout endpoint validant le test /notes;

--- notes-server/src/app.ts
+++ notes-server/src/app.ts
@@ -7,5 +7,8 @@ type Response = express.Response;
 app.get('/', (req: Request, res: Response) => {
     res.json('hello world');
 })
+app.get('/notes', (req: Request, res: Response) => {
+    res.json('all the notes');
+})
 
 export = app;
